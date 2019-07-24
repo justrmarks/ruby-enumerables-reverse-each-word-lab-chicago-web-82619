5fa7196a8aa754281b4831252aaa40b4cb25aa2d
Justice Marks <her@shedaddy.com>
2019-07-24 21:40:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   
   result
 end
+
+def reverse_each_word(string) 
+  
+  arrString = string.split(" ")
+  
+  result = arrString.each{|word| word.reverse!}.join(" ")
+  
+  result
+end
