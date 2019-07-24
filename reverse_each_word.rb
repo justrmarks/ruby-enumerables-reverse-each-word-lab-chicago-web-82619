89800bf17da52cc9666b6c6ec258ecd71e28626f
Justice Marks <her@shedaddy.com>
2019-07-24 21:41:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,16 +2,9 @@ def reverse_each_word(string)
   
   arrString = string.split(" ")
   
-  result = arrString.each{|word| word.reverse!}.join(" ")
+  result = arrString.collect{|word| word.reverse!}.join(" ")
   
   result
 end
 
-def reverse_each_word(string) 
-  
-  arrString = string.split(" ")
-  
-  result = arrString.each{|word| word.reverse!}.join(" ")
-  
-  result
-end
+
